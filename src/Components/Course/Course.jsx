4b0aabe9e7a8439009c7388c5570bca99fc18752
Maha4Gaber <77496313+Maha4Gaber@users.noreply.github.com>
2024-03-27 20:09:47 +0200

    course

--- src/Components/Course/Course.jsx
+++ src/Components/Course/Course.jsx
@@ -1,35 +1,38 @@
-import React from 'react';
-import classes from './Course.module.css';
-import CourseImg from '../../assests/couresImg.jpg';
-import Love from '../../assests/Love';
-import BtnRead from '../BtnRead/BtnRead';
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
-import ArrowRight from '../../assests/ArrowRight';
-import { useTranslation } from 'react-i18next';
-function Course({ price }) {
-    const {t} = useTranslation()
-  return (
-      <motion.div className={classes.course} data-aos="flip-right">
-          <div className={classes.course__container}>
-              <div className={classes.container__tag}>
-                  <span>
-                      <Love />
-                  </span>
-                  <button>{t("EBTS")}</button>
-              </div>
-              <img src={CourseImg} alt="courseImg" />
-          </div>
-          <h4>{t("DAES")}</h4>
-          <hr />
-          <div className={classes.course__tag}>
-              <span>{price ?? t("free")}</span>
-              <Link to="/course-details" className={classes.btnRead}>
-                {t("readMore")} &nbsp; <ArrowRight />
-              </Link>
-          </div>
-      </motion.div>
-  );
+import React from "react";
+import classes from "./Course.module.css";
+import CourseImg from "../../assests/couresImg.jpg";
+import Love from "../../assests/Love";
+import BtnRead from "../BtnRead/BtnRead";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import ArrowRight from "../../assests/ArrowRight";
+import { useTranslation } from "react-i18next";
+function Course({ price ,course }) {
+const { t ,i18n} = useTranslation();
+const lang = i18n.language;
+let isEn = lang == "en" ?true: false;
+// console.log(course);
+return (
+<motion.div className={classes.course} data-aos="flip-right">
+    <div className={classes.course__container}>
+    <div className={classes.container__tag}>
+        <span>
+        <Love />
+        </span>
+        <button>{course.category}</button>
+    </div>
+    <img src={course.image} alt="courseImg" />
+    </div>
+    <h4>{isEn?course.title_en:course.title_ar}</h4>
+    <hr />
+    <div className={classes.course__tag}>
+    <span>{course.type=='free'? t("free") :course.price}</span>
+    <Link to="/course-details" className={classes.btnRead}>
+        {t("readMore")} &nbsp; <ArrowRight />
+    </Link>
+    </div>
+</motion.div>
+);
 }
 
 export default Course;
--- src/Components/courses/Courses.jsx
+++ src/Components/courses/Courses.jsx
@@ -1,56 +1,65 @@
-import React from 'react';
-import classes from './Courses.module.css';
-import Course from '../Course/Course';
-import { useTranslation } from 'react-i18next';
+import React, { useState } from "react";
+import classes from "./Courses.module.css";
+import Course from "../Course/Course";
+import { useTranslation } from "react-i18next";
+import axios from "axios";
 function Courses() {
-    const {t} = useTranslation()
-    return (
-        <>
-            <div data-aos="zoom-in" className={classes.coures_content}>
-                <h5 className="text-center">{t("eduPaths")}</h5>
-                <h3 className="text-center">
-                    {t("eduPathsSubTitle")}
-                </h3>
-                <ul>
-          <li className={classes.active}>{t("all")}</li>
-          <li>{t("educationAndDevelopment")}</li>
-          <li>{t("educationAndDevelopment")}</li>
-          <li>{t("eduSystemDesign")}</li>
-        </ul>
-        <ul>
-          <li>{t("teacherEvaluation")}</li>
-          <li>{t("threeLevelHost")}</li>
-        </ul>
-      </div>
+const { t } = useTranslation();
+const [courses, setCourses] = useState([]);
+async function getdata() {
+await axios.get("api/v1/courses/").then((res) => {
+    console.log(res.data);
+    setCourses(res.data);
+});
+}
+getdata();
+return (
+<>
+    <div data-aos="zoom-in" className={classes.coures_content}>
+    <h5 className="text-center">{t("eduPaths")}</h5>
+    <h3 className="text-center">{t("eduPathsSubTitle")}</h3>
+    <ul>
+        <li className={classes.active}>{t("all")}</li>
+        <li>{t("educationAndDevelopment")}</li>
+        <li>{t("educationAndDevelopment")}</li>
+        <li>{t("eduSystemDesign")}</li>
+    </ul>
+    <ul>
+        <li>{t("teacherEvaluation")}</li>
+        <li>{t("threeLevelHost")}</li>
+    </ul>
+    </div>
 
-            <div className="row p-5">
-                <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-                    <Course />
-                </div>
-                <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-                    <Course />
-                </div>
-                <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-                    <Course />
-                </div>
-                <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-                    <Course />
-                </div>
-                <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-                    <Course />
-                </div>
-                <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-                    <Course />
-                </div>
-                <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-                    <Course />
-                </div>
-                <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-                    <Course />
-                </div>
-            </div>
-        </>
-    );
+    <div className="row p-5">
+        {courses.map((card, idx) => (
+        <div key={idx} className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+            <Course course={card} />
+        </div>
+        ))}
+    {/* <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+    <Course />
+</div>
+<div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+    <Course />
+</div>
+<div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+    <Course />
+</div>
+<div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+    <Course />
+</div>
+<div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+    <Course />
+</div>
+<div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+    <Course />
+</div>
+<div className="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+    <Course />
+</div> */}
+    </div>
+</>
+);
 }
 
 export default Courses;
