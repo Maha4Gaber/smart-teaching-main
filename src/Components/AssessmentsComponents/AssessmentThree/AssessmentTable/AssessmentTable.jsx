72a7a4effa5409625298a37f97c837cd383f8588
Maha4Gaber <77496313+Maha4Gaber@users.noreply.github.com>
2024-03-29 11:52:11 +0200

    speddmotor

--- src/Components/AssessmentsComponents/AssessmentThree/AssessmentTable/AssessmentTable.jsx
+++ src/Components/AssessmentsComponents/AssessmentThree/AssessmentTable/AssessmentTable.jsx
@@ -9,311 +9,335 @@ import { Chart } from "react-google-charts";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 const AssessmentTable = ({
-Questions,
-columnHead = null,
-tableName,
-adjustCell,
-btnName,
+  Questions,
+  columnHead = null,
+  tableName,
+  adjustCell,
+  btnName,
 }) => {
-  const {t,i18n} = useTranslation()
+  const { t, i18n } = useTranslation();
   const lang = i18n.language;
-  let isRTL = lang =='en'?'ltr':'rtl'
-  let isright=lang =='en'?'5%':'auto'
+  let isRTL = lang == "en" ? "ltr" : "rtl";
+  let isright = lang == "en" ? "5%" : "auto";
   const tableStyles = {
     direction: isRTL,
-    left:isright
-    };
-// const [showError, setShowError] = useState(false);
-const navigate=useNavigate()
-const [selectedAnswers, setSelectedAnswers] = useState({});
-const check = () => columnHead;
-const recievddata=[
-  ["Element", "evaluation", { role: "style" },{ role: 'annotation' }],
-]
-const answeredQuestions = [];
-const categoryAverages = {};
-const categoryQuestionCounts = {};
+    left: isright,
+  };
+  // const [showError, setShowError] = useState(false);
+  const navigate = useNavigate();
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+  const check = () => columnHead;
+  const recievddata = [
+    ["Element", "evaluation", { role: "style" }, { role: "annotation" }],
+  ];
+  const answeredQuestions = [];
+  const categoryAverages = {};
+  const categoryQuestionCounts = {};
 
-const handleAnswerClick = (questionId, answerId) => {
-  setSelectedAnswers((prevAnswers) => ({
-    ...prevAnswers,
-    [questionId]: answerId,
-  }));
-};
-// console.log();
+  const handleAnswerClick = (questionId, answerId) => {
+    setSelectedAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      [questionId]: answerId,
+    }));
+  };
+  // console.log();
 
-  let url = 'api/v1/TCA/'
+  let url = "api/v1/TCA/";
   let cat = "ABCDEFGH";
+  let nav = true;
 
-  if(Questions[0].category==='ass3cat1'){
-    url='api/v1/COI/'
-    cat='ABCD'
+  if (Questions[0].category === "ass3cat1") {
+    url = "api/v1/COI/";
+    cat = "ABCD";
+    nav = false;
+  } else if (Questions[0].category === "ass5p1cat1") {
+    url = "api/v1/SRT/";
+    cat = "ABCDEFG";
+    nav = false;
   }
-  else if(Questions[0].category==='ass5p1cat1'){
-    url='api/v1/SRT/'
-    cat='ABCDEFG'
-  }
-async function handleSendData(values) {
-  try {
-    let { data } = await axios.post(url, values);
+  async function handleSendData(values) {
+    try {
+      let { data } = await axios.post(url, values);
 
-    if (data) {
-      console.log(data);
-      let newdata=data.data.percentage_level_score
-      cat = cat.split("");
-      cat.forEach((el,index)=>{
-        let color ='blue'
-        
-        if (newdata[el][0]>25 && newdata[el][0] <37.5) {
-          color='red'
-        } else if(newdata[el][0]>37.5 && newdata[el][0] <62.5) {
-          color='yellow'
-          
-        }
-        else if(newdata[el][0]<87.5 && newdata[el][0] >62.5) {
-          color='green'
-        }
-          recievddata.push([t(Questions[index]['category']),newdata[el][0],
-            color,newdata[el][1]
-          ])
-        })
+      if (data) {
+        // console.log(data);
+        let newdata = data.data.percentage_level_score;
+        cat = cat.split("");
+        cat.forEach((el, index) => {
+          let color = "blue";
 
-        localStorage.data=JSON.stringify(recievddata)
-        navigate('/pdf')
-      console.log(recievddata);
-    }
-  } catch (err) {
-    console.log(err);
-  }
-}
-const printSelectedAnswers = () => {
-  Questions.forEach((category, index) => {
-    let total = 0;
-    let answeredCount = 0;
-    let cat = "ABCDEFGH";
-    cat = cat.split("");
-    category.questions.forEach((question) => {
-      const answerId = selectedAnswers[question.id];
-
-      if (answerId !== undefined) {
-        answeredCount++;
-        total += answerId;
-        answeredQuestions.push({
-          category: cat[index] + "_score",
-          question: question.text,
-          answer: answerId,
+          if (newdata[el][0] > 25 && newdata[el][0] < 37.5) {
+            color = "red";
+          } else if (newdata[el][0] > 37.5 && newdata[el][0] < 62.5) {
+            color = "yellow";
+          } else if (newdata[el][0] < 87.5 && newdata[el][0] > 62.5) {
+            color = "green";
+          }
+          recievddata.push([
+            t(Questions[index]["category"]),
+            newdata[el][0],
+            color,
+            newdata[el][1],
+          ]);
         });
+
+        localStorage.data = JSON.stringify(recievddata);
+        setTimeout(() => {
+          nav ?? navigate("/pdf");
+        }, 2000);
+        // console.log(recievddata);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  const printSelectedAnswers = () => {
+    Questions.forEach((category, index) => {
+      let total = 0;
+      let answeredCount = 0;
+      let cat = "ABCDEFGH";
+      cat = cat.split("");
+      category.questions.forEach((question) => {
+        const answerId = selectedAnswers[question.id];
+
+        if (answerId !== undefined) {
+          answeredCount++;
+          total += answerId;
+          answeredQuestions.push({
+            category: cat[index] + "_score",
+            question: question.text,
+            answer: answerId,
+          });
+        }
+      });
+
+      if (answeredCount > 0) {
+        let avg = total / answeredCount;
+        avg = avg.toString();
+        avg = avg.slice(0, 2);
+        let numavg = Number(avg);
+        categoryAverages[cat[index] + "_score"] = numavg;
+        categoryQuestionCounts[cat[index] + "_score"] = answeredCount;
       }
     });
 
-    if (answeredCount > 0) {
-      let avg = total / answeredCount;
-      avg = avg.toString();
-      avg = avg.slice(0, 2);
-      let numavg = Number(avg);
-      categoryAverages[cat[index] + "_score"] = numavg;
-      categoryQuestionCounts[cat[index] + "_score"] = answeredCount;
+    if (
+      answeredQuestions.length ===
+      Questions[Questions.length - 1].questions[
+        Questions[Questions.length - 1].questions.length - 1
+      ].id
+    ) {
+      // console.log('Selected Answers:', answeredQuestions);
+      // console.log("Category Averages:", categoryAverages);
+      handleSendData(categoryAverages);
+      // console.log('Category Question Counts:', categoryQuestionCounts);
+    } else {
+      console.log("Please answer all questions.");
     }
+  };
+
+  const isCalculateButtonDisabled = () => {
+    return Questions.some((category) =>
+      category.questions.some(
+        (question) => selectedAnswers[question.id] === undefined
+      )
+    );
+  };
+  // console.log(Questions);
+
+  // console.log(data);
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
   });
 
-  if (
-    answeredQuestions.length ===
-    Questions[Questions.length - 1].questions[
-      Questions[Questions.length - 1].questions.length - 1
-    ].id
-  ) {
-    // console.log('Selected Answers:', answeredQuestions);
-    console.log("Category Averages:", categoryAverages);
-    handleSendData(categoryAverages);
-    // console.log('Category Question Counts:', categoryQuestionCounts);
-  } else {
-    console.log("Please answer all questions.");
-  }
-};
-
-const isCalculateButtonDisabled = () => {
-  return Questions.some((category) =>
-    category.questions.some(
-      (question) => selectedAnswers[question.id] === undefined
-    )
-  );
-};
-console.log(Questions);
-
-
-// console.log(data);
-const componentRef = useRef();
-const handlePrint = useReactToPrint({
-  content: () => componentRef.current,
-});
-
-return (
-  <div>
-    {columnHead ? (
-      <table className="table table-bordered ass-table"  style={tableStyles}>
-        <thead>
-          <tr>
-            <td className="position-relative W-23 W-22 W-17 W-18">
-              {" "}
-              <div className="custom-th position-absolute top-50 start-50 translate-middle">
-                {columnHead.head}
-              </div>{" "}
-            </td>
-            <th rowSpan="2" className="custom-th W-60">
-              {" "}
-              {columnHead.title}{" "}
-            </th>
-            {columnHead.answers.map(({ answer }, idx) => (
-              <td key={idx} className="text-center position-relative W-5 W-3">
-                <div className="position-absolute top-50 start-50 translate-middle">
-                  {answer}
-                </div>
-              </td>
-            ))}
-          </tr>
-        </thead>
-
-        <tbody>
-          {Questions.map(({ category, answers, questions }, idx) => (
-            <React.Fragment key={idx}>
-              {questions.map((question, idx) => (
-                <tr key={idx}>
-                  {idx === 0 && (
-                    <td
-                      rowSpan={questions.length}
-                      className="W-28 W-23 W-22 W-13 W-18"
-                    >
-                      <div className="text-dark ">{t(category)}</div>
-                    </td>
-                  )}
-
-                  <th  className="W-60" scope="col">
-                    <div style={tableStyles}>{t(question.text)}</div>
-                  </th>
-
-                  {[1, 2, 3, 4].map((answerId) => (
-                    <td
-                      key={answerId}
-                      onClick={() => handleAnswerClick(question.id, answerId)}
-                      className={
-                        selectedAnswers[question.id] === answerId
-                          ? "selected text-center cursor-pointer W-5 W-3"
-                          : "cursor-pointer W-5 W-3"
-                      }
-                    >
-                      {selectedAnswers[question.id] === answerId && (
-                        <div >
-                          <FaCheck />
-                        </div>
-                      )}
-                    </td>
-                  ))}
-                </tr>
-              ))}
-            </React.Fragment>
-          ))}
-        </tbody>
-      </table>
-    ) : (
-      <table className="table table-bordered ass-table" style={tableStyles}>
-        {tableName && (
+  return (
+    <div>
+      {columnHead ? (
+        <table className="table table-bordered ass-table" style={tableStyles}>
           <thead>
             <tr>
-              <th
-                colSpan={Questions[0].answers.length + 1}
-                className={adjustCell && `p-3 py-4`}
-              >
-                {adjustCell ? (
-                  <span className=" fw-medium ">{tableName.head}</span>
-                ) : (
-                  <div className="L-3">{tableName.head}</div>
-                )}{" "}
+              <td className="position-relative W-23 W-22 W-17 W-18">
+                {" "}
+                <div className="custom-th position-absolute top-50 start-50 translate-middle">
+                  {columnHead.head}
+                </div>{" "}
+              </td>
+              <th rowSpan="2" className="custom-th W-60">
+                {" "}
+                {columnHead.title}{" "}
               </th>
+              {columnHead.answers.map(({ answer }, idx) => (
+                <td key={idx} className="text-center position-relative W-5 W-3">
+                  <div className="position-absolute top-50 start-50 translate-middle">
+                    {answer}
+                  </div>
+                </td>
+              ))}
             </tr>
           </thead>
-        )}
 
-        {Questions.map(({ category, answers, questions }, idx) => (
-          <React.Fragment key={idx}>
+          <tbody>
+            {Questions.map(({ category, answers, questions }, idx) => (
+              <React.Fragment key={idx}>
+                {questions.map((question, idx) => (
+                  <tr key={idx}>
+                    {idx === 0 && (
+                      <td
+                        rowSpan={questions.length}
+                        className="W-28 W-23 W-22 W-13 W-18"
+                      >
+                        <div className="text-dark ">{t(category)}</div>
+                      </td>
+                    )}
+
+                    <th className="W-60" scope="col">
+                      <div style={tableStyles}>{t(question.text)}</div>
+                    </th>
+
+                    {[1, 2, 3, 4].map((answerId) => (
+                      <td
+                        key={answerId}
+                        onClick={() => handleAnswerClick(question.id, answerId)}
+                        className={
+                          selectedAnswers[question.id] === answerId
+                            ? "selected text-center cursor-pointer W-5 W-3"
+                            : "cursor-pointer W-5 W-3"
+                        }
+                      >
+                        {selectedAnswers[question.id] === answerId && (
+                          <div>
+                            <FaCheck />
+                          </div>
+                        )}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </React.Fragment>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <table className="table table-bordered ass-table" style={tableStyles}>
+          {tableName && (
             <thead>
               <tr>
-                <th scope="col" className={adjustCell && `p-3`}>
-                  {" "}
+                <th
+                  colSpan={Questions[0].answers.length + 1}
+                  className={adjustCell && `p-3 py-4`}
+                >
                   {adjustCell ? (
-                    <span>{t(category)}</span>
+                    <span className=" fw-medium ">{tableName.head}</span>
                   ) : (
-                    <div>{t(category)}</div>
+                    <div className="L-3">{tableName.head}</div>
                   )}{" "}
                 </th>
-
-                {answers.map(({ answer }, idx) => (
-                  <td key={idx} className="text-center position-relative">
-                    {adjustCell ? (
-                      <div className="position-absolute top-50 start-50 translate-middle">
-                        {answer}
-                      </div>
-                    ) : (
-                      <span>{answer}</span>
-                    )}
-                  </td>
-                ))}
               </tr>
             </thead>
+          )}
 
-            <tbody>
-              {questions.map((question, idx) => (
-                <tr key={idx}>
-                  <th scope="col" className={adjustCell && `py-3 px-3`}>
+          {Questions.map(({ category, answers, questions }, idx) => (
+            <React.Fragment key={idx}>
+              <thead>
+                <tr>
+                  <th scope="col" className={adjustCell && `p-3`}>
+                    {" "}
                     {adjustCell ? (
-                      <span> {t(question.text)}</span>
+                      <span>{t(category)}</span>
                     ) : (
-                      <div>  {t(question.text)}</div>
-                    )}
+                      <div>{t(category)}</div>
+                    )}{" "}
                   </th>
 
-                  {[1, 2, 3, 4].map((answerId) => (
-                    <td
-                      key={answerId}
-                      onClick={() => handleAnswerClick(question.id, answerId)}
-                      className={
-                        selectedAnswers[question.id] === answerId
-                          ? "selected text-center cursor-pointer"
-                          : "cursor-pointer"
-                      }
-                    >
-                      {selectedAnswers[question.id] === answerId && (
-                        <div>
-                          <FaCheck />
+                  {answers.map(({ answer }, idx) => (
+                    <td key={idx} className="text-center position-relative">
+                      {adjustCell ? (
+                        <div className="position-absolute top-50 start-50 translate-middle">
+                          {answer}
                         </div>
+                      ) : (
+                        <span>{answer}</span>
                       )}
                     </td>
                   ))}
                 </tr>
-              ))}
-            </tbody>
-          </React.Fragment>
-        ))}
-      </table>
-    )}
+              </thead>
 
-    <button
-      className="submit-ass"
-      onClick={printSelectedAnswers}
-      disabled={isCalculateButtonDisabled()}
-    >
-      {t('submit')}
-      
-    </button>
+              <tbody>
+                {questions.map((question, idx) => (
+                  <tr key={idx}>
+                    <th scope="col" className={adjustCell && `py-3 px-3`}>
+                      {adjustCell ? (
+                        <span> {t(question.text)}</span>
+                      ) : (
+                        <div> {t(question.text)}</div>
+                      )}
+                    </th>
 
-    {/* {showError && (
+                    {[1, 2, 3, 4].map((answerId) => (
+                      <td
+                        key={answerId}
+                        onClick={() => handleAnswerClick(question.id, answerId)}
+                        className={
+                          selectedAnswers[question.id] === answerId
+                            ? "selected text-center cursor-pointer"
+                            : "cursor-pointer"
+                        }
+                      >
+                        {selectedAnswers[question.id] === answerId && (
+                          <div>
+                            <FaCheck />
+                          </div>
+                        )}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </React.Fragment>
+          ))}
+        </table>
+      )}
+
+      <button
+        className="submit-ass"
+        onClick={printSelectedAnswers}
+        disabled={isCalculateButtonDisabled()}
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModalToggle"
+      >
+        {t("submit")}
+      </button>
+        <div className="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+            Evaluation completed successfully
+            </div>
+            <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* {nav ?? (
+      )} */}
+      {/* {showError && (
       <div className="alert alert-danger error my-2 py-2">
           Please answer all questions*
       </div>
       )} */}
-
-
-  </div>
-);
+    </div>
+  );
 };
 
 export default AssessmentTable;
--- src/Components/SpeedMoter/SpeedMoter.jsx
+++ src/Components/SpeedMoter/SpeedMoter.jsx
@@ -13,65 +13,48 @@ import ArrowImg from "../../assests/Arrow.png";
 import { useTranslation } from "react-i18next";
 import axios from "axios";
 
-
 function SpeedMoter() {
   const { t, i18n } = useTranslation();
   const lang = i18n.language;
-  let isEn = lang == "en" ?true: false;
+  let isEn = lang == "en" ? true : false;
   const [rate, setRate] = useState([]);
-  const [value, setValue] = useState(1);
-  const [rank, setRank] = useState(0);
+  const [value, setValue] = useState(-0.9);
+  const [rank, setRank] = useState(7);
   const [soi, setSoi] = useState("6%");
   const [inful, setInful] = useState("ADHD");
-  
-  async function getdata(){
-    await axios.get('api/v3/influences/')
-    .then(res=>{
-      setRate(res.data)
-      setValue(res.data[0].size);
-      setRank(res.data[0].rank);
-    })
-  }
-  getdata()
-  let newValue = 0;
-  function handleStudent() {
-    newValue = (100 * 1.44) / 4;
-    setValue((value) => newValue);
-  }
-  function handleTeacher() {
-    newValue = (100 * 2.5) / 4;
-    setValue((value) => newValue);
-  }
-  function HandleHome() {
-    newValue = (100 * 3.44) / 4;
-    setValue((value) => newValue);
-  }
-   useEffect(() => {
-    let item = rate.filter((item) => item.influence === inful);
+
+  function handleStudent(inful) {
+    let item = rate.filter((item) => item.influence == inful);
     let singleItem = item[0];
-    console.log(item);
-    setTimeout(() => {
-      setRank(singleItem.rank);
-      setValue(singleItem.size);
-      if (singleItem.category_en.toLowerCase() === "student") {
-        // setSoi("57%");
-        isEn?setSoi('48%'):setSoi("48%")
-      } else if (singleItem.category_en.toLowerCase() === "home") {
-        isEn?setSoi('7%'):setSoi("90%")
-        // setSoi("23%");
-      } else if (singleItem.category_en.toLowerCase() === "teaching") {
-        isEn?setSoi('90%'):setSoi("6%")
+    setRank(singleItem.rank);
+    let s = Number(singleItem.size);
+    setValue(s);
+    if (singleItem.category_en.toLowerCase() === "student") {
+      isEn ? setSoi("48%") : setSoi("48%");
+    } else if (singleItem.category_en.toLowerCase() === "home") {
+      isEn ? setSoi("7%") : setSoi("90%");
+    } else if (singleItem.category_en.toLowerCase() === "teaching") {
+      isEn ? setSoi("90%") : setSoi("6%");
+    } else if (singleItem.category_en.toLowerCase() === "teacher") {
+      isEn ? setSoi("69%") : setSoi("27%");
+    } else if (singleItem.category_en.toLowerCase() === "school") {
+      isEn ? setSoi("27%") : setSoi("69%");
+    }
+  }
+
+  useEffect(() => {
+    const getdata = async () => {
+      try {
+        await axios.get("api/v3/influences/").then((res) => {
+          setRate(res.data);
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
-      else if (singleItem.category_en.toLowerCase() === "teacher") {
-        isEn?setSoi('69%'):setSoi("27%")
-        // setSoi("23%");
-      }
-      else if (singleItem.category_en.toLowerCase() === "school") {
-        isEn?setSoi('27%'):setSoi("69%")
-        // setSoi("23%");
-      }
-    }, 500);
-  }, [inful]);
+    };
+
+    getdata();
+  }, []);
   return (
     <section className={classes.influncers}>
       <h6>{t("learnImpactOfFactors")}</h6>
@@ -90,9 +73,18 @@ function SpeedMoter() {
       </div>
       <div className={classes.inf__selcets}>
         <ResultInf title="rank" value={rank} />
-        <select value={inful} onChange={(e) => setInful(e.target.value)}>
-            {rate.map((item,id) => (
-            <option key={id} value={item.influence}>{isEn?item.influence_en:item.influence_ar}</option>
+        <select
+          value={inful}
+          className={classes.select}
+          onChange={(e) => {
+            setInful(e.target.value);
+            handleStudent(inful);
+          }}
+        >
+          {rate.map((item, id) => (
+            <option key={id} value={item.influence}>
+              {isEn ? item.influence_en : item.influence_ar}
+            </option>
           ))}
         </select>
         <ResultInf title="Effect Size" value={value} />
@@ -105,7 +97,7 @@ function SpeedMoter() {
           segments={4}
           minValue={-1}
           segmentColors={["firebrick", "tomato", "gold", "limegreen"]}
-          customSegmentStops={[-1,-.5, 0,1,2]}
+          customSegmentStops={[-1, -0.5, 0, 1, 2]}
           textColor="#000"
         />
       </div>
@@ -114,10 +106,12 @@ function SpeedMoter() {
 }
 
 export default SpeedMoter;
-{/* <ReactSpeedometer
+{
+  /* <ReactSpeedometer
           value={value}
           maxValue={6}
           maxSegmentLabels={5}
           segments={4}
           textColor="#000"
-        /> */}
+        /> */
+}
